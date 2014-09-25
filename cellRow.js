f26ffbbf3fd872583862cc7029828f66d1dd1106
Guillermo Vargas <gjvargas@mit.edu>
2014-09-25 10:53:04 -0400

    adding week 1 stuff

--- cellRow.js
+++ cellRow.js
@@ -11,6 +11,20 @@ function rowWidget(onChange) {
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
   ];
+  var previous = [
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+  ];
+  var matrixHeight = 10;
+  var matrixWidth = 10;
   var matrix = Array.create(function(j) {
     var row = Array.create(function(i) {
       return $("<span>")
@@ -62,6 +76,59 @@ function rowWidget(onChange) {
     return $("<div>").append(row);
   }, 10)
 
+  function step() {
+    // Load the previous state so we can load the next state
+    // into organisms. Here I use previous as a temporary variable so that I can
+    // alter organisms reliably. I considered using closures to pass the matrix,
+    // updating each cell based on the local matrix, thus requiring only 1 matrix.
+    // Ultimately I decided against it, because 2 global matrices seemed preferrable
+    // to thousands/millions of temporary variable in terms of performance. Is there
+    // a way I might use closures to eliminate this second state (previous) and
+    // maintaining good performance?
+    for (var i = 0; i < matrixHeight; i++) {
+      for (var j = 0; j < matrixWidth; j++) {
+        previous[i][j] = life[i][j];
+      }
+    }
+
+    //update each cell based on the number of neighbors and its previous state
+    for (var i = 0; i < matrixHeight; i++) {
+      for (var j = 0; j < matrixWidth; j++) {
+        // Switch case is faster than if/else in this case because
+        // there are several possibilities
+        switch(numNeighbors(i, j)) {
+          case 0:
+          case 1:
+              life[i][j] = 0;
+          case 2:
+              break;
+          case 3:
+              life[i][j] = 1;
+              break;
+          default:
+              life[i][j] = 0;
+        }
+      }
+    }
+    for(var x = 0; x < matrixHeight; x++) {
+      for(var y = 0; y < matrixWidth; y++) {
+        window.setTimeout(updateSprite(x,y))
+      }
+    }
+
+  }
+  // Helper function that determines the number of neighbors a cell
+  // had last turn for any given coordinate.
+  function numNeighbors(i, j) {
+    return previous[(i+matrixHeight-1)%matrixHeight][(j+matrixWidth-1)%matrixWidth]
+          + previous[i][(j+matrixWidth-1)%matrixWidth]
+          + previous[(i+1)%matrixHeight][(j+matrixWidth-1)%matrixWidth]
+          + previous[(i+matrixHeight-1)%matrixHeight][j]
+          + previous[(i+1)%matrixHeight][j]
+          + previous[(i+matrixHeight-1)%matrixHeight][(j+1)%matrixWidth]
+          + previous[i][(j+1)%matrixWidth]
+          + previous[(i+1)%matrixHeight][(j+1)%matrixWidth];
+  }
   return $("<div>").append(matrix);
 }
 
